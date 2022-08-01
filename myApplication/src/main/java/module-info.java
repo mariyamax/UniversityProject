08c24f3520cb53e8d9add86ed4ac968da33bc4be
mariyamax <89139747+mariyamax@users.noreply.github.com>
2022-08-01 14:11:30 +0300

    Delete myApplication directory

--- myApplication/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.myapplication {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.myapplication to javafx.fxml;
-    exports com.example.myapplication;
-}
